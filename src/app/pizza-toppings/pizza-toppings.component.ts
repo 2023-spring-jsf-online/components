a3dec87e66023b99d01ed2b280c793be596c07df
Austin Brown <austinnbrown95@gmail.com>
2023-03-06 16:12:41 -0600

    Calling a DI'd service
    next lets show the pizza toppings in the component

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
   styleUrls: ['./pizza-toppings.component.css']
 })
-export class PizzaToppingsComponent {
+export class PizzaToppingsComponent implements OnInit {
 
+  // Magic DI or dependency Inhjection
+  constructor(
+    private pizzSvc: PizzaService
+  ) {}
+
+  ngOnInit(): void {
+
+    const pt = this.pizzSvc.getPizzaToppingsFromTheCloud();
+    console.log(pt);
+    
+  }
 }
