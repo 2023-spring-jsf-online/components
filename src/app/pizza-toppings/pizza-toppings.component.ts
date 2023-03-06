a2af465cbd89a60d6f5b8cec8eec7ebca3dd3834
Austin Brown <austinnbrown95@gmail.com>
2023-03-06 16:25:43 -0600

    Added Pizza toppings display interface..
    and transformed the data to be the proper shape

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../pizza.service';
 
+interface PizzaToppingDisplay {
+  name: string;
+  price: number;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
@@ -13,10 +19,19 @@ export class PizzaToppingsComponent implements OnInit {
     private pizzSvc: PizzaService
   ) {}
 
+  availablePizzaToppings: PizzaToppingDisplay[] = [];
   ngOnInit(): void {
 
     const pt = this.pizzSvc.getPizzaToppingsFromTheCloud();
     console.log(pt);
+
+    this.availablePizzaToppings = pt.map(
+      x => ({
+        ...x
+        , checked: false
+      })
+    );
     
+    console.log(this.availablePizzaToppings);
   }
 }
