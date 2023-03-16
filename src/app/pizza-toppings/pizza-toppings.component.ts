d89edac67034e99fd96660f252121895d3e3f378
Ascher <atownsend3@madisoncollege.edu>
2023-03-15 21:15:55 -0500

    Calculate Total

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -31,6 +31,12 @@ ngOnInit(): void {
   );
 
   console.log(this.availablePizzaToppings);
-}
+};
 
-}
+totalPrice = 0;
+
+calculateTotal = () => {
+  this.totalPrice = this.availablePizzaToppings.filter(x => x.checked).reduce((acc, x) => acc + x.price, 0);
+};
+
+};
